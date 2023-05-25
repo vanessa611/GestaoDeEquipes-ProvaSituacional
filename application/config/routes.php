3bfeb893ef83f39cf8f87cb36047976727731b30
Vanessa Correia Gomes <83028430+vanessa611@users.noreply.github.com>
2023-05-25 20:20:15 -0300

    Create routes.php

---
+++ application/config/routes.php
@@ -0,0 +1,5 @@
+$route['product'] = 'product/index';
+$route['product/create'] = 'product/create';
+$route['product/edit/(:num)'] = 'product/edit/$1';
+$route['product/update/(:num)'] = 'product/update/$1';
+$route['product/delete/(:num)'] = 'product/delete/$1';
